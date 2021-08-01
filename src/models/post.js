42466238c59940fe667f30f939a2f51fa016df07
ahmadhuss <ahmadnet600@gmail.com>
2021-08-02 02:31:35 +0500

    Updated config

--- src/models/post.js
+++ src/models/post.js
@@ -10,8 +10,6 @@ const db = new Sequelize(
   process.env.DATABASE_URL,
   {
     dialect: 'postgres',
-    ssl: true,
-    protocol: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
