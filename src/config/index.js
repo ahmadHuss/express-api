4e2e0236364911d6aa42c467e2cbdca5b3cc66d4
ahmadhuss <ahmadnet600@gmail.com>
2021-08-02 02:03:50 +0500

    Updated config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,13 @@
 export default {
   db: {
     dialect: 'postgres', // 'mysql'|'sqlite'|'postgres'|'mssql'
-    host: 'https://express-rest-00.herokuapp.com/', // Your host, by default is localhost
+    host: 'https://express-rest-00.herokuapp.com', // Your host, by default is localhost
     database: 'd4akfurhist5n0', // Your database name
     user: 'jzyfgccgincddz', // Your Postgres user, by default is postgres
-    password: 'ce3c05c89979a0398f14df23bf14adef66989dcbe534dc08d61a54a0e4c5f298'// Your Db password, sometimes by default
+    password: 'ce3c05c89979a0398f14df23bf14adef66989dcbe534dc08d61a54a0e4c5f298',// Your Db password, sometimes by default
     //is empty.
+    dialectOptions: {
+      ssl: true
+    }
   }
 };
