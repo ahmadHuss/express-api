ef667a5428b5e4e913e1891b1f50322a7babd2d6
ahmadhuss <ahmadnet600@gmail.com>
2021-08-02 08:02:36 +0500

    Updated default config

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,9 @@ export default {
   db: {
     dialect: 'postgres', // 'mysql'|'sqlite'|'postgres'|'mssql'
     host: 'localhost', // Your host, by default is localhost
-    database: 'd4akfurhist5n0', // Your database name
-    user: 'jzyfgccgincddz', // Your Postgres user, by default is postgres
-    password: 'ce3c05c89979a0398f14df23bf14adef66989dcbe534dc08d61a54a0e4c5f298',// Your Db password, sometimes by default
+    database: 'test', // Your database name
+    user: 'postgres', // Your Postgres user, by default is postgres
+    password: 'a',// Your Db password, sometimes by default
     //is empty.
-    dialectOptions: {
-      ssl: true
-    }
   }
 };
